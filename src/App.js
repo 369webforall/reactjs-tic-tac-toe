24d2f8b4d6bfbb581e97fb3c1328244ca0a672f4
369webforall <369webforall@gmail.com>
2023-05-17 16:01:02 +0545

    update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Board from './components/Board';
+function App() {
+  return (
+    <div className="App">
+      <h1>Tic Tac Toe Game</h1>
+
+      <Board />
+    </div>
+  );
+}
+
+export default App;
